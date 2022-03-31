df2fc332683774250d54023b27391f563ce95ba8
Luís Ferreira <contact@lsferreira.net>
2022-03-31 19:27:42 +0100

    fix(pre_commit): use pre_commit_fork package name
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- pre_commit/constants.py
+++ pre_commit/constants.py
@@ -15,7 +15,7 @@ INSTALLED_STATE_VERSION = '1'
 # Bump when modifying `empty_template`
 LOCAL_REPO_VERSION = '1'
 
-VERSION = importlib_metadata.version('pre_commit')
+VERSION = importlib_metadata.version('pre_commit_fork')
 
 # `manual` is not invoked by any installed git hook.  See #719
 STAGES = (
